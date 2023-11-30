0b8fcd18ce523ffd29ffdaf5bacee8009906d882
Rômulo Oliveira <romuloffall@gmail.com>
2023-11-30 14:59:57 -0300

    #add new routes and security routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
 import ViewEditNote from "@/views/ViewEditNote.vue";
+import ViewAuth from "@/views/ViewAuth.vue";
+import { useStoreAuth } from "@/stores/storeAuth";
 
 const routes = [
   {
@@ -19,11 +21,27 @@ const routes = [
     name: "stats",
     component: ViewStats,
   },
+  {
+    path: "/auth",
+    name: "auth",
+    component: ViewAuth,
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  const storeAuth = useStoreAuth();
+  if (!storeAuth.user?.id && to.name !== "auth") {
+    return { name: "auth" };
+  }
+
+  if (storeAuth.user?.id && to.name === "auth") {
+    return { name: "notes" };
+  }
 });
 
 export default router;
