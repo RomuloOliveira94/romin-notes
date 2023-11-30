84e681ef868e8a2d669eca56c8a7315917474c82
Rômulo Oliveira <romuloffall@gmail.com>
2023-11-30 14:59:43 -0300

    #fix nav bar and routing

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,8 +1,14 @@
 <template>
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-primary py-2" role="navigation" aria-label="main navigation">
     <div class="container is-max-desktop px-2">
       <div class="navbar-brand">
-        <img src="../../../public/rnotes-nobg.png" width="150" />
+        <RouterLink @click="showMobileNav = false" to="/">
+          <img
+            src="../../../public/rnotes-nobg.png"
+            class="image"
+            width="150"
+          />
+        </RouterLink>
         <a
           role="button"
           class="navbar-burger"
@@ -25,6 +31,33 @@
         ref="navBarRef"
       >
         <div class="navbar-start">
+          <!-- <RouterLink
+            @click="showMobileNav = false"
+            class="navbar-item"
+            to="/"
+            active-class="is active"
+          >
+            Notes
+          </RouterLink>
+          <RouterLink
+            @click="showMobileNav = false"
+            class="navbar-item"
+            to="/stats"
+            active-class="is active"
+          >
+            Stats
+          </RouterLink> -->
+          <div class="navbar-item">
+            <button
+              class="button is-small is-warning"
+              @click.prevent="logout"
+              v-if="storeAuth.user?.id"
+            >
+              Log out {{ storeAuth.user?.email }}
+            </button>
+          </div>
+        </div>
+        <div class="navbar-end">
           <RouterLink
             @click="showMobileNav = false"
             class="navbar-item"
@@ -41,16 +74,26 @@
           >
             Stats
           </RouterLink>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-warning">
+          <!-- <div class="buttons">
+             <RouterLink
+                @click="showMobileNav = false"
+                class="navbar-item button is-warning"
+                to="/auth"
+                active-class="is active"
+                v-if="!storeAuth.user?.id"
+              >
                 <strong>Sign up</strong>
-              </a>
-              <a class="button is-light"> Log in </a>
-            </div>
-          </div>
+              </RouterLink>
+              <RouterLink
+                @click="showMobileNav = false"
+                class="navbar-item button is-warning"
+                to="/auth"
+                active-class="is active"
+                v-if="!storeAuth.user?.id"
+              >
+                <strong>Login</strong>
+              </RouterLink>
+            </div> -->
         </div>
       </div>
     </div>
@@ -58,24 +101,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { onClickOutside } from "@vueuse/core";
+  import { ref } from "vue";
+  import { onClickOutside } from "@vueuse/core";
+  import { useStoreAuth } from "@/stores/storeAuth";
 
-const showMobileNav = ref(false);
+  const storeAuth = useStoreAuth();
 
-const navBarRef = ref(null);
-const burgerMenuRef = ref(null);
-onClickOutside(navBarRef, () => (showMobileNav.value = false), {
-  ignore: [burgerMenuRef],
-});
+  const showMobileNav = ref(false);
+
+  const navBarRef = ref(null);
+  const burgerMenuRef = ref(null);
+  onClickOutside(navBarRef, () => (showMobileNav.value = false), {
+    ignore: [burgerMenuRef],
+  });
+
+  const logout = () => {
+    storeAuth.logoutUser();
+    showMobileNav.value = false;
+  };
 </script>
 
 <style>
-@media (max-width: 1023px) {
-  .navbar-menu {
-    position: absolute;
-    left: 0;
-    width: 100%;
+  @media (max-width: 1023px) {
+    .navbar-menu {
+      position: absolute;
+      left: 0;
+      width: 100%;
+    }
   }
-}
 </style>
