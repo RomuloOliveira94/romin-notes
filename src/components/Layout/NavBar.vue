c712cb140b7146a3f432c8c25822bfccae3e4360
Rômulo Oliveira <romuloffall@gmail.com>
2023-11-30 22:20:31 -0300

    #implement function and button to change language

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,5 +1,9 @@
 <template>
-  <nav class="navbar is-primary py-2" role="navigation" aria-label="main navigation">
+  <nav
+    class="navbar is-primary py-2"
+    role="navigation"
+    aria-label="main navigation"
+  >
     <div class="container is-max-desktop px-2">
       <div class="navbar-brand">
         <RouterLink @click="showMobileNav = false" to="/">
@@ -37,7 +41,7 @@
               @click.prevent="logout"
               v-if="storeAuth.user?.id"
             >
-              Log out {{ storeAuth.user?.email }}
+              {{ $t("auth.logout") }} {{ storeAuth.user?.email }}
             </button>
           </div>
         </div>
@@ -48,7 +52,7 @@
             to="/"
             active-class="is active"
           >
-            Notes
+            {{ $t("buttons.title") }}
           </RouterLink>
           <RouterLink
             @click="showMobileNav = false"
@@ -56,8 +60,17 @@
             to="/stats"
             active-class="is active"
           >
-            Stats
+            {{ $t("stats.head_title_stats") }}
           </RouterLink>
+          <div
+            class="navbar-item"
+            v-for="sLocale in supportedLocales"
+            :key="`locale-${sLocale}`"
+          >
+            <div @click="switchLanguage(sLocale)" class="lang-selector">
+              <country-flag :country="filterLocale(sLocale)" size="normal" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -68,12 +81,29 @@
   import { ref } from "vue";
   import { onClickOutside } from "@vueuse/core";
   import { useStoreAuth } from "@/stores/storeAuth";
+  import { useStoreLang } from "@/stores/storeLang";
+  import CountryFlag from "vue-country-flag-next";
 
   const storeAuth = useStoreAuth();
+  const storeLang = useStoreLang();
+
+  const supportedLocales = storeLang.supportedLocales;
+
+  const filterLocale = (locale) => {
+    if (locale === "en") return "us";
+    if (locale === "pt") return "br";
+  };
+
+  const switchLanguage = async (locale) => {
+    const newLocale = locale;
+    await storeLang.switchLanguage(newLocale);
+    showMobileNav.value = false;
+  };
 
   const showMobileNav = ref(false);
 
   const navBarRef = ref(null);
+
   const burgerMenuRef = ref(null);
   onClickOutside(navBarRef, () => (showMobileNav.value = false), {
     ignore: [burgerMenuRef],
@@ -92,5 +122,9 @@
       left: 0;
       width: 100%;
     }
+  }
+
+  .lang-selector {
+    cursor: pointer;
   }
 </style>
